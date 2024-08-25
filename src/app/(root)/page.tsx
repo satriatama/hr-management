c31bd0cf0fa92cfcc203dee01e2a98ce1a72060f
satriatama <satriatama121@gmail.com>
2024-08-25 22:39:16 +0700

    initial commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='Home'>
+        <div className='Home-Container'>
+            <div className='Home-Container-Left'>
+                <h1>HRMS</h1>
+                <p>Modern HR Management System</p>
+            </div>
+            <div className='Home-Container-Right'>
+                <img src='/icons/logo.svg' alt='HRMS Logo' />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home
